c398056f893612b09c8366663a17c33ea17940c2
Harr-izle <harrietbuadee@gmail.com>
2024-06-11 19:51:00 +0000

    capitalization function

---
+++ labWork.js
@@ -0,0 +1,7 @@
+// String Transformation
+
+//Capitalization
+function capitalizeFirstLetter(str){
+    return str.charAt(0).toUpperCase() + str.slice(1, str.length)
+}
+console.log(capitalizeFirstLetter("harrizle"))
