ca8e8eeb567adc3b3e4ed5253865bc5b6e9c9223
Harr-izle <harrietbuadee@gmail.com>
2024-06-11 21:02:56 +0000

    reverse funtion

--- labWork.js
+++ labWork.js
@@ -1,7 +1,22 @@
 // String Transformation
 
 //Capitalization
-function capitalizeFirstLetter(str){
-    return str.charAt(0).toUpperCase() + str.slice(1, str.length)
-}
-console.log(capitalizeFirstLetter("harrizle"))
+// function capitalizeFirstLetter(str){
+//     return str.charAt(0).toUpperCase() + str.slice(1, str.length)
+// }
+// console.log(capitalizeFirstLetter("harrizle"))
+
+//Reversing a string
+const reverse = (str) => {
+  if (typeof str !== 'string' || str.length === 0) {
+    return "invalid input";
+  }
+
+  let result = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    result += str[i];
+  }
+
+  return result;
+};
+console.log(reverse("Harrizle"))
