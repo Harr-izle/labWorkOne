d12041c67097fc5b4227ab2ab2fabc635a4146c7
Harr-izle <harrietbuadee@gmail.com>
2024-06-11 21:52:41 +0000

    uncommented capitalization code

--- labWork.js
+++ labWork.js
@@ -1,10 +1,10 @@
 // String Transformation
 
 //Capitalization
-// function capitalizeFirstLetter(str){
-//     return str.charAt(0).toUpperCase() + str.slice(1, str.length)
-// }
-// console.log(capitalizeFirstLetter("harrizle"))
+function capitalizeFirstLetter(str){
+    return str.charAt(0).toUpperCase() + str.slice(1, str.length)
+}
+console.log(capitalizeFirstLetter("harrizle"))
 
 //Reversing a string
 const reverse = (str) => {
